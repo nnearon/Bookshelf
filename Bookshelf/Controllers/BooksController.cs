80b4300be614a8515e72e272c187be7e1e52bdf1
nnearon <135106898+nnearon@users.noreply.github.com>
2023-07-07 18:37:07 +0300

    BooksController.cs

--- Bookshelf/Controllers/BooksController.cs
+++ Bookshelf/Controllers/BooksController.cs
@@ -8,6 +8,8 @@ namespace BookshelfAPI.Controllers
     [Route("api/[controller]")]
     public class BooksController : ControllerBase
     {
+        // A static list to store the books
+
         private static List<Book> books = new List<Book>
         {
             new Book { Id = 1, Title = "Atomic Habits", Author = "James Clear", Year = 2018 },
@@ -15,17 +17,27 @@ namespace BookshelfAPI.Controllers
             new Book { Id = 3, Title = "The Distance Home", Author = "Paula Saunders", Year = 2018 }
         };
 
+        // GET: api/Books
+
         [HttpGet]
         public IActionResult GetAllBooks()
         {
+            // Return all the books as a response
             return Ok(books);
         }
+
+        // POST: api/Books
 
         [HttpPost]
         public IActionResult AddBook([FromBody] Book book)
         {
+            // Assign a new ID to the book based on the count of existing books
             book.Id = books.Count + 1;
+
+            // Add the book to the list
             books.Add(book);
+
+            // Return the newly added book as a response
             return Ok(book);
         }
     }
